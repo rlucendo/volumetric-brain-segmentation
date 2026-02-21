d948229b137b1449368998a7f3015826520f8890
rlucendo <r.lucendo@hotmail.com>
2026-02-21 17:47:26 +0100

    fix: change checkpoint folder path to absolute

--- scripts/train.py
+++ scripts/train.py
@@ -38,7 +38,8 @@ def main(config_dir: str):
     # 5. Setup Callbacks
     # Save the model with the highest validation Dice score
     checkpoint_callback = ModelCheckpoint(
-        dirpath="checkpoints/",
+        # Save directly to mounted Google Drive if running in Colab, else local
+        dirpath="/content/drive/MyDrive/Colab Notebooks/volumetric-brain-segmentation/checkpoints/" if os.path.exists("/content/drive") else "checkpoints/",
         filename="brats-{epoch:02d}-{val/dice_score:.4f}",
         monitor="val/dice_score",
         mode="max",
